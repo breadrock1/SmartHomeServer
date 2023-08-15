4868931a976390d4ed7a9b863a1fcc641be2e4ea
Bread White <breadrock1@gmail.com>
2023-08-15 21:39:16 +0300

    Add local changes to server crate

--- smart_home_server/src/handlers.rs
+++ smart_home_server/src/handlers.rs
@@ -1,10 +1,6 @@
-use crate::errors::{RecvError, RecvResult};
-use crate::errors::{SendError, SendResult};
 use smart_home_socket::room::Room;
 use smart_home_socket::thermometer::ThermometerSession;
 use std::str::{FromStr, Split};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpStream;
 
 pub struct Request<'a>(Split<'a, &'a str>);
 
@@ -152,39 +148,5 @@ impl UdpRequestHandler {
             Ok(_) => format!("Successful set data {}", new_value),
             Err(e) => format!("Failed: {}", e),
         }
-    }
-}
-
-pub async fn send_request(stream: &mut TcpStream, str_data: String) -> SendResult {
-    let output_buffer = str_data.as_bytes();
-    let buffer_length = output_buffer.len() as u32;
-    let result = stream.write_all(&buffer_length.to_be_bytes()).await;
-    if result.is_err() {
-        let error = result.err().unwrap();
-        return Err(SendError::Io(error));
-    }
-    match stream.write_all(output_buffer).await {
-        Ok(_) => Ok(str_data),
-        Err(e) => Err(SendError::Io(e)),
-    }
-}
-
-pub async fn recv_request(stream: &mut TcpStream) -> RecvResult {
-    let mut input_buffer = [0; 4];
-    let _ = stream
-        .read(&mut input_buffer)
-        .await
-        .map_err(|e| RecvError::ReadData(e.to_string()));
-    let length = u32::from_be_bytes(input_buffer);
-
-    let mut input_buffer = vec![0; length as _];
-    let _ = stream
-        .read(&mut input_buffer)
-        .await
-        .map_err(|e| RecvError::ReadData(e.to_string()));
-
-    match String::from_utf8(input_buffer) {
-        Ok(s) => Ok(s),
-        Err(e) => Err(RecvError::ReadData(e.to_string())),
     }
 }
--- smart_home_server/src/lib.rs
+++ smart_home_server/src/lib.rs
@@ -1,2 +1,73 @@
+use crate::errors::{RecvError, RecvResult, SendError, SendResult};
+use std::io::{Read, Write};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net;
+
 pub mod errors;
 pub mod handlers;
+
+pub async fn send_request_async(stream: &mut net::TcpStream, str_data: String) -> SendResult {
+    let output_buffer = str_data.as_bytes();
+    let buffer_length = output_buffer.len() as u32;
+    let result = stream.write_all(&buffer_length.to_be_bytes()).await;
+    if result.is_err() {
+        let error = result.err().unwrap();
+        return Err(SendError::Io(error));
+    }
+    match stream.write_all(output_buffer).await {
+        Ok(_) => Ok(str_data),
+        Err(e) => Err(SendError::Io(e)),
+    }
+}
+
+pub fn send_request(stream: &mut std::net::TcpStream, str_data: String) -> SendResult {
+    let output_buffer = str_data.as_bytes();
+    let buffer_length = output_buffer.len() as u32;
+    let result = stream.write_all(&buffer_length.to_be_bytes());
+    if result.is_err() {
+        let error = result.err().unwrap();
+        return Err(SendError::Io(error));
+    }
+    match stream.write_all(output_buffer) {
+        Ok(_) => Ok(str_data),
+        Err(e) => Err(SendError::Io(e)),
+    }
+}
+
+pub async fn recv_request_async(stream: &mut net::TcpStream) -> RecvResult {
+    let mut input_buffer = [0; 4];
+    let _ = stream
+        .read(&mut input_buffer)
+        .await
+        .map_err(|e| RecvError::ReadData(e.to_string()));
+    let length = u32::from_be_bytes(input_buffer);
+
+    let mut input_buffer = vec![0; length as _];
+    let _ = stream
+        .read(&mut input_buffer)
+        .await
+        .map_err(|e| RecvError::ReadData(e.to_string()));
+
+    match String::from_utf8(input_buffer) {
+        Ok(s) => Ok(s),
+        Err(e) => Err(RecvError::ReadData(e.to_string())),
+    }
+}
+
+pub fn recv_request(stream: &mut std::net::TcpStream) -> RecvResult {
+    let mut input_buffer = [0; 4];
+    let _ = stream
+        .read(&mut input_buffer)
+        .map_err(|e| RecvError::ReadData(e.to_string()));
+    let length = u32::from_be_bytes(input_buffer);
+
+    let mut input_buffer = vec![0; length as _];
+    let _ = stream
+        .read(&mut input_buffer)
+        .map_err(|e| RecvError::ReadData(e.to_string()));
+
+    match String::from_utf8(input_buffer) {
+        Ok(s) => Ok(s),
+        Err(e) => Err(RecvError::ReadData(e.to_string())),
+    }
+}
